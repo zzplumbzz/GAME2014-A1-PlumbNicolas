1a89020174501841f69910e2a5e2db0f82350e9d
zzplumbzz <nicolasplumb@yahoo.com>
2020-10-04 21:20:27 -0400

    initial commit

---
+++ Assets/MyScripts/HowToPlayButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HowToPlayButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnHowToPlayButtonPressed()
+    {
+        Debug.Log("OnHowToPlayButton Pressed");
+        SceneManager.LoadScene("HowToPlay");
+    }
+}
---
+++ Assets/MyScripts/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBarScript healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //test damage.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
